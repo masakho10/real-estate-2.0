2863b1766469884e64aa21a69d378604f2edddd8
Malickmimd <malickdiene@esp.sn>
2024-06-29 21:36:26 +0000

    last fix

--- app/(routes)/user/page.jsx
+++ app/(routes)/user/page.jsx
@@ -7,10 +7,10 @@ import UserListing from './_components/UserListing'
 function User() {
   return (
     <div className='my-6 md:px-10 lg:px-32 w-full'>
-        <h2 className='font-bold text-2xl py-3'>Profile</h2>
+        <h2 className='font-bold text-2xl py-3'>Profil</h2>
         <UserProfile>
             <UserButton.UserProfilePage
-            label='My Listing'
+            label='Ma liste'
             labelIcon={<Building2 className='h-5 w-5' />}
             url="my-listing"
             >
